aca17033dbe40a962a3c19490532403ad2749c6a
AlekseyMitlex <aamtr@yandex.ru>
2021-11-18 17:58:38 +0700

    implement HW_1

---
+++ Lesson 1/Variable.java
@@ -0,0 +1,23 @@
+public class Variable {
+
+    public static void main(String[] args) {
+        byte osName = 10;
+        short intelCore = 7;
+        int cores = 4;
+        long ram = 12;
+        float cpu = 3.50F;
+        double virtualMemory = 14.7;
+        char logicalProc = '8';
+        boolean cdromYes = true;
+        boolean cdromNo = false;
+
+        System.out.println ("Operating system: " + osName);
+        System.out.println ("IntelCore: " + intelCore);
+        System.out.println ("Cores: " + cores);
+        System.out.println ("Ram: " + ram);
+        System.out.println ("Cpu: " + cpu);
+        System.out.println ("VirtualMemory: " + virtualMemory);
+        System.out.println ("Logical processor: " + logicalProc);
+        System.out.println ("Cdrom: " + cdromNo);
+    }
+}
