5d01a2b7e365e53def8edab15d77c9c17697db4e
AlekseyMitlex <aamtr@yandex.ru>
2021-11-23 20:48:07 +0700

    Cycle Lesson1/8 with edit 2

--- Lesson 1/Cycle.java
+++ Lesson 1/Cycle.java
@@ -1,37 +1,31 @@
 public class Cycle {
     
     public static void main(String[] args) {
-        System.out.println(" ");
-        System.out.println("все числа от [0, 20]: ");
-        System.out.println(" ");
+        System.out.println("\nвсе числа от [0, 20]:\n");
 
-            for (int i = 0; i <=20 ; i++) {
-                System.out.println(i);
-            }
+        for (int i = 0; i <= 20; i++) {
+            System.out.println(i);
+        }
 
-        System.out.println(" ");
-        System.out.println("все числа от [6, -6] (с шагом итерации 2): ");
-        System.out.println(" ");
+        System.out.println("\nвсе числа от [6, -6] (с шагом итерации 2): \n");
 
-            int j = 6;
-            while (j >= -6) {
-                System.out.println(j);
-                j = j - 2;
-            }
+        int j = 6;
+        while (j >= -6) {
+            System.out.println(j);
+            j = j - 2;
+        }
 
-        System.out.println(" ");
-        System.out.println("сумму нечетных чисел от [10, 20]:");
-        System.out.println(" ");
+        System.out.println("\nсумму нечетных чисел от [10, 20]:\n");
 
-            int k = 11;
-            int sum = 0;
-            do {
-                if(k % 2 != 0) {
+        int k = 11;
+        int sum = 0;
+        do {
+            if (k % 2 != 0) {
                 sum = sum + k;
-                    System.out.println("Числа не четные: " + k);
-                }
-                k++;
-            } while (k <=20);
-                    System.out.println(sum);
+                System.out.println("Числа не четные: " + k);
+            }
+            k++;
+        } while (k <= 20);
+        System.out.println(sum);
     }
 }
