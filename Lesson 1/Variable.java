72f79050f74590d9056f237f73f96289a3555f64
AlekseyMitlex <aamtr@yandex.ru>
2021-11-27 09:24:01 +0700

    Variable Lesson1 edit2

--- Lesson 1/Variable.java
+++ Lesson 1/Variable.java
@@ -8,16 +8,16 @@ public class Variable {
         float cpu = 3.50F;
         double virtualMemory = 14.7;
         char logicalProc = '8';
-        boolean cdRomYES = true;
-        boolean cdRomNO = false;
+        boolean cdRomYes = true;
+        boolean cdRomNo = false;
 
-        System.out.println ("Operating system: " + osName);
-        System.out.println ("IntelCore: " + intelCore);
-        System.out.println ("Cores: " + cores);
-        System.out.println ("Ram: " + ram);
-        System.out.println ("Cpu: " + cpu);
-        System.out.println ("VirtualMemory: " + virtualMemory);
-        System.out.println ("Logical processor: " + logicalProc);
-        System.out.println ("Cdrom: " + cdromNo);
+        System.out.println("Operating system: " + osName);
+        System.out.println("IntelCore: " + intelCore);
+        System.out.println("Cores: " + cores);
+        System.out.println("Ram: " + ram);
+        System.out.println("Cpu: " + cpu);
+        System.out.println("VirtualMemory: " + virtualMemory);
+        System.out.println("Logical processor: " + logicalProc);
+        System.out.println("Cdrom: " + cdRomNo);
     }
 }
