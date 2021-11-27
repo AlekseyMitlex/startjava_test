9df74a49a7a05f3ef3a988afa08c013dcbe101e0
AlekseyMitlex <aamtr@yandex.ru>
2021-11-27 13:49:58 +0700

    Cycle Lesson1/10 Calculator edit1

--- Lesson 1/Calculator.java
+++ Lesson 1/Calculator.java
@@ -1,36 +1,38 @@
 public class Calculator {
+    
     public static void main(String[] args) {
         int a = 6;
         int b = 2;
-        int c = 7;
-        char x = '^'; // Остальные арифметические операции
+        char calcSign = '^'; // Остальные арифметические операции
 
         int sum;
         int sub;
         int mult;
         int divis;
         int remains;
-        int caret;
 
-        sum = a + b;
-        sub = a - b;
-        mult = a * b;
-        divis = a / b;
-        remains = c % b;
-        caret = a ^ b;
-
-        if (x == '+') {
+        if (calcSign == '+') {
+            sum = a + b;
             System.out.println("При использовании + :" + sum);
-        } else if(x == '-') {
+        } else if (calcSign == '-') {
+            sub = a - b;
             System.out.println("При использовании - :" + sub);
-        } else if(x == '-') {
+        } else if (calcSign == '*') {
+            mult = a * b;
             System.out.println("При использовании * :" + mult);
-        } else if(x == '/') {
+        } else if (calcSign == '/') {
+            divis = a / b;
             System.out.println("При использовании / :" + divis);
-        } else if(x == '%') {
+        } else if (calcSign == '%') {
+            remains = a % b;
             System.out.println("При использовании % :" + remains);
-        } else if(x == '^') {
-            System.out.println("При использовании ^ :" + caret);
-        } 
+        } else if (calcSign == '^') {
+            int degree = 1;
+            while (b > 0) {
+                degree *= a;
+                b--;
+            }
+            System.out.println("При использовании ^ :" + degree);
+        }
     }
 }
