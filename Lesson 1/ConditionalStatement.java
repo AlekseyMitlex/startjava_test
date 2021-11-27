189dcd8175e56ee3b3ab437fb5cf237770a9ddaa
AlekseyMitlex <aamtr@yandex.ru>
2021-11-27 09:40:13 +0700

    ConditionalStatement Lesson1/7 with edits 3

--- Lesson 1/ConditionalStatement.java
+++ Lesson 1/ConditionalStatement.java
@@ -1,8 +1,7 @@
 public class ConditionalStatement {
 
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         int age = 40;
-        
         if (age > 20) {
             System.out.println("Возраст верен");
         }
