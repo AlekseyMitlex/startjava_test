98eb50094a1441a3e35c06c2e4935b344a04217a
AlekseyMitlex <aamtr@yandex.ru>
2021-11-28 19:16:45 +0700

    Lesson1/10 Calculator edit2

--- Lesson 1/Calculator.java
+++ Lesson 1/Calculator.java
@@ -3,30 +3,26 @@ public class Calculator {
     public static void main(String[] args) {
         int a = 6;
         int b = 2;
-        char calcSign = '^'; // Остальные арифметические операции
+        char sign = '/'; // (+, -, *, /, ^, %) Подставляем знаки
 
-        int sum;
-        int sub;
-        int mult;
-        int divis;
-        int remains;
+        int result;
 
-        if (calcSign == '+') {
-            sum = a + b;
-            System.out.println("При использовании + :" + sum);
-        } else if (calcSign == '-') {
-            sub = a - b;
-            System.out.println("При использовании - :" + sub);
-        } else if (calcSign == '*') {
-            mult = a * b;
-            System.out.println("При использовании * :" + mult);
-        } else if (calcSign == '/') {
-            divis = a / b;
-            System.out.println("При использовании / :" + divis);
-        } else if (calcSign == '%') {
-            remains = a % b;
-            System.out.println("При использовании % :" + remains);
-        } else if (calcSign == '^') {
+        if (sign == '+') {
+            result = a + b;
+            System.out.println("Результат вычисления = " + result);
+        } else if (sign == '-') {
+            result = a - b;
+            System.out.println("Результат вычисления = " + result);
+        } else if (sign == '*') {
+            result = a * b;
+            System.out.println("Результат вычисления = " + result);
+        } else if (sign == '/') {
+            result = a / b;
+            System.out.println("Результат вычисления = " + result);
+        } else if (sign == '%') {
+            result = a % b;
+            System.out.println("Результат вычисления = " + result);
+        } else if (sign == '^') {
             int degree = 1;
             while (b > 0) {
                 degree *= a;
