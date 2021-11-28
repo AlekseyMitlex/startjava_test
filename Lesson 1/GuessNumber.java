3baddda2218b468085a2e41d78175cd240d50f67
AlekseyMitlex <aamtr@yandex.ru>
2021-11-28 19:04:58 +0700

    Lesson1/10 GuessNumber edit1

--- Lesson 1/GuessNumber.java
+++ Lesson 1/GuessNumber.java
@@ -1,19 +1,18 @@
 public class GuessNumber {
 
     public static void main(String[] args) {
-        int number = 30; // загаданное число (0-100]
-        int user = 70;    // Число игрока
+        int secretNumber = 30; // загаданное число (0-100]
+        int playerNumber = 70;    // Число игрока
 
-        while (user < number) {
-            System.out.println(user + " Данное число больше того, что загадал компьютер");
-            user++;
+        while (playerNumber != secretNumber) {
+            if(playerNumber > secretNumber) {
+                System.out.println(playerNumber + " Данное число меньше того, что загадал компьютер");
+                playerNumber--;
+            } else if(playerNumber < secretNumber) {
+                System.out.println(playerNumber + " Данное число меньше того, что загадал компьютер");
+                playerNumber++;
+            }
         }
-        while (user > number) {
-            System.out.println(user + " Данное число меньше того, что загадал компьютер");
-            user--;
-        }
-        if (user == number)  {
-            System.out.println("Поздравляю, число угадано!!! " + user);
-        }
+        System.out.println("Поздравляю, число угадано! " + playerNumber);
     }
 }
