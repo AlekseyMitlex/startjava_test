ce578187352638fe392f69205d6197872a09c9db
AlekseyMitlex <aamtr@yandex.ru>
2021-11-29 18:29:09 +0700

    Lesson1/10 Calculator edit3

--- Lesson 1/Calculator.java
+++ Lesson 1/Calculator.java
@@ -3,7 +3,7 @@ public class Calculator {
     public static void main(String[] args) {
         int a = 6;
         int b = 2;
-        char sign = '/'; // (+, -, *, /, ^, %) Подставляем знаки
+        char sign = '^'; // (+, -, *, /, ^, %) Подставляем знаки
 
         int result;
 
@@ -23,10 +23,12 @@ public class Calculator {
             result = a % b;
             System.out.println("Результат вычисления = " + result);
         } else if (sign == '^') {
+            if (b == 0) {
+                System.out.println(1);
+            }
             int degree = 1;
-            while (b > 0) {
-                degree *= a;
-                b--;
+            for (int i = 0; i < b ; i++) {
+                degree = degree * a;
             }
             System.out.println("При использовании ^ :" + degree);
         }
