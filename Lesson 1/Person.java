f5e3802ebeb81669a3181ba32d3b95ffd4e0e446
AlekseyMitlex <aamtr@yandex.ru>
2021-11-30 16:49:37 +0700

    Person Lesson2/1 edit

---
+++ Lesson 1/Person.java
@@ -0,0 +1,28 @@
+public class  Person {
+    
+    String  pol = "Man";
+    String name = "Ivan";
+    float height = 1.80F;
+    float weight = 85;
+    byte age = 27;
+
+    void move() {
+        System.out.println("moving");
+    }
+
+    boolean isSit() {
+        return true;
+    }
+
+    boolean isRun() {
+        return false;
+    }
+
+    String tell() {
+        return "Helo";
+    }
+
+    boolean isLearnJava() {
+        return true;
+    }
+}
