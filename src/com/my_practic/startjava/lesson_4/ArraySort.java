fa5642a3d8b4feb1b9ce4ef01ac9e59d0117f994
AlekseyMitlex <aamtr@yandex.ru>
2021-12-15 07:48:39 +0700

    Lection Alishev+Maks2

--- src/com/my_practic/startjava/lesson_4/ArraySort.java
+++ src/com/my_practic/startjava/lesson_4/ArraySort.java
@@ -4,19 +4,15 @@ import java.util.Arrays;
 
 public class ArraySort {
     public static void main(String[] args) {
-            int[] array = {10, 3, 53, 2, 5, 7, 3, 6, 12, 3, 32, 65}; //исходный массив
-            // Сортировка пузырьком
-            int temp;                                               // переменная для замены
-            for (int i = array.length - 1; i > 0; i--) {            // внешний цикл
-                for (int j = 0; j < i; j++) {                       // внутренний цикл
-                    if (array[j] > array[j + 1]) {                  // сравнение
-                        temp = array[j];                            // если истино, то меняем
-                        array[j] = array[j + 1];                    // местами элементы
-                        array[j + 1] = temp;
-                    }
-                }
-            }
-            // конец сортировки
-            System.out.println(Arrays.toString(array));             //выводим результат на консоль
+        int array[] = {1, 9, 2, -4, 5, 12};
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+
+        Arrays.sort(array);
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+
     }
 }
