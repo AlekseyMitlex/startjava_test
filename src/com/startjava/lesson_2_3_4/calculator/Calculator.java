6655ed41a3a425ed6238cc5ddd8f8922a05dc8ed
AlekseyMitlex <aamtr@yandex.ru>
2021-12-22 20:52:39 +0700

    calculator RETURN с yield final

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -7,10 +7,10 @@ public class Calculator {
     private static char sign;
 
     public static double calculate(String mathExpression) {
-        String[] numbers = mathExpression.split(" ");
-        firstNumber = Integer.parseInt(numbers[0]);
-        sign = numbers[1].charAt(0);
-        secondNumber = Integer.parseInt(numbers[2]);
+        String[] splittedExpression = mathExpression.split(" ");
+        firstNumber = Integer.parseInt(splittedExpression[0]);
+        sign = splittedExpression[1].charAt(0);
+        secondNumber = Integer.parseInt(splittedExpression[2]);
 
         return switch (sign) {
             case '+' -> Math.addExact(firstNumber, secondNumber);
@@ -19,28 +19,10 @@ public class Calculator {
             case '/' -> Math.floorDiv(firstNumber, secondNumber);
             case '%' -> Math.floorMod(firstNumber, secondNumber);
             case '^' -> Math.pow(firstNumber, secondNumber);
-            default -> 0;
-        } ;
+            default -> {
+                System.out.println("Не то нажал");
+                yield 0;
+            }
+        };
     }
 }
-
-//switch (sign) {
-//        case '+' : return Math.addExact(firstNumber, secondNumber);
-//        case '-' : return Math.subtractExact(firstNumber, secondNumber);
-//        case '*' : return Math.multiplyExact(firstNumber, secondNumber);
-//        case '/' : return Math.floorDiv(firstNumber, secondNumber);
-//        case '%' : return Math.floorMod(firstNumber, secondNumber);
-//        case '^' : return Math.pow(firstNumber, secondNumber);
-//        }
-//        return 0;
-
-//switch (sign) {
-//        case '+' -> result = Math.addExact(firstNumber, secondNumber);
-//        case '-' -> result = Math.subtractExact(firstNumber, secondNumber);
-//        case '*' -> result = Math.multiplyExact(firstNumber, secondNumber);
-//        case '/' -> result = Math.floorDiv(firstNumber, secondNumber);
-//        case '%' -> result = Math.floorMod(firstNumber, secondNumber);
-//        case '^' -> result = Math.pow(firstNumber, secondNumber);
-//default -> System.out.println("Не то нажал");
-//        }
-//        return result;
