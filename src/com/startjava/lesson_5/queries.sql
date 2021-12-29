6b19dd7405c298b46318608ccaba233c55fa1522
AlekseyMitlex <aamtr@yandex.ru>
2021-12-29 08:49:47 +0700

    baza + 3 faila finish Jaeger

--- src/com/startjava/lesson_5/queries.sql
+++ src/com/startjava/lesson_5/queries.sql
@@ -26,5 +26,43 @@ INSERT INTO Jaegers(modelName, mark, height, weight, origin, launch, kaijuKill)
 		('BracerPhoenix',	'Mark-5',	'70.91',	'2.128',	'Japan',	'2025',	'3'),
 		('BrawlerYukon',	'Mark-1',	'73.3',		'2.075',	'USA',		'2015',	'4');
 
+-- output the complete table
 
+SELECT * FROM jaegers;
 
+-- display only non-destroyed robots
+
+SELECT * FROM jaegers WHERE launch >= '2025';
+
+-- display robots of several series,  Mark-1 and Mark-6
+
+SELECT * FROM jaegers WHERE mark = 'Mark-1' UNION
+SELECT * FROM jaegers WHERE mark = 'Mark-6' ORDER BY mark DESC;
+
+-- sort the table in descending order by column mark
+
+SELECT * FROM jaegers ORDER BY mark DESC;
+
+-- display the oldest robot
+
+SELECT * FROM jaegers WHERE launch = (SELECT MIN(launch) FROM jaegers);
+
+-- display robots that have destroyed more / less than all kaiju
+
+    -- less
+SELECT * FROM jaegers WHERE kaijukill = (SELECT MIN(kaijukill) FROM jaegers);
+
+    -- more
+SELECT * FROM jaegers WHERE kaijukill = (SELECT MAX(kaijukill) FROM jaegers);
+
+-- display the average weight of robots
+
+SELECT AVG(weight) FROM jaegers;
+
+-- increase by one the number of destroyed kaiju robots that have not yet been destroyed
+
+UPDATE jaegers SET kaijukill = kaijukill + 1 WHERE launch > 2022;
+
+-- remove destroyed robots
+
+postgres=# DELETE FROM jaegers WHERE launch < 2022;
