d40c9b83a3a9868d9485fb683535e93fca6c97ea
AlekseyMitlex <aamtr@yandex.ru>
2021-12-30 17:51:55 +0700

    baza + 3 faila finish Jaeger + Destroyer

--- src/com/startjava/lesson_5/init_db.sql
+++ src/com/startjava/lesson_5/init_db.sql
@@ -4,12 +4,12 @@ INSERT INTO Jaegers(modelName, mark, height, weight, status, origin, launch, kai
 	VALUES
 		('GipsyDanger',		'Mark-3',	'79.25',	'1.980',    'Destroyed', 	'USA',		'2017',	'9'),
 		('ChernoAlpha',		'Mark-1',	'85.34',	'2.412',    'Worker',    	'Russia',	'2015',	'6'),
-		('StrikerEureka',	'Mark-5',	'76.2',		'1.850',    'Worker',       'Australia','2019',	'11'),
+		('StrikerEureka',	'Mark-5',	'76.2',		'1.850',    'Worker',          'Australia','2019',	'11'),
 		('GipsyAvenger',	'Mark-6',	'81.77',	'2.004',    'Destroyed', 	'Unknown',	'2034',	'1'),
 		('CrimsonTyphoon',	'Mark-4',	'76.2',		'1.722',    'Worker',    	'China',	'2018',	'7'),
 		('ObsidianFury',	'Mark-6',	'85.0',		'1.800',    'Destroyed', 	'Unknown',	'2035',	'0'),
 		('CoyoteTango',		'Mark-1',	'85.35',	'2.312',    'Worker',   	'Japan',	'2015',	'2'),
-		('Apex',            'Mark-6',	'86.0',		'1.870',    'Worker',   	'Unknown',	'2035',	'0'),
-		('BracerPhoenix',	'Mark-5',	'70.91',	'2.128',    'Worker',       'Japan',	'2025',	'3'),
-		('BrawlerYukon',	'Mark-1',	'73.3',		'2.075',    'Destroyed',    'USA',		'2015',	'4');
+		('Apex',                'Mark-6',	'86.0',		'1.870',    'Worker',   	'Unknown',	'2035',	'0'),
+		('BracerPhoenix',	'Mark-5',	'70.91',	'2.128',    'Worker',           'Japan',	'2025',	'3'),
+		('BrawlerYukon',	'Mark-1',	'73.3',		'2.075',    'Destroyed',        'USA',		'2015',	'4');
 
