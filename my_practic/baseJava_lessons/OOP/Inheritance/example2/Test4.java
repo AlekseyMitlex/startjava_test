30cf3a78c4575375342439305d91d407cd6b3756
AlekseyMitlex <aamtr@yandex.ru>
2022-01-10 21:08:55 +0700

    add examples for encapsulation/interface

---
+++ my_practic/baseJava_lessons/OOP/Inheritance/example2/Test4.java
@@ -0,0 +1,45 @@
+package baseJava_lessons.OOP.Inheritance.example2;
+
+public class Test4 {
+    public static void main(String[] args) {
+        Jumpable j1 = new Human();
+        Jumpable j2 = new Animal();
+    }
+}
+
+class Human implements Jumpable {
+
+    @Override
+    public void jump() {
+        System.out.println("Human jumps");
+    }
+}
+
+
+class Animal implements Jumpable {
+
+    @Override
+    public void jump() {
+        System.out.println("Animal jumps");
+    }
+}
+
+interface Jumpable {
+
+    void jump();
+}
+
+// interface может наследовать интерфейсы
+interface A2 extends Jumpable {
+    void abc();
+}
+
+interface B2 extends A2, Jumpable {
+    void def();
+}
+
+// Абстрактт класс может наследовать конкретный или абстрактный класс
+// и имплементировать 2 и более интерфейса
+// A2 включено в B2 соответственно поэтому можно не имплеиентить А2
+abstract class D implements B2 {
+}
---
+++ my_practic/baseJava_lessons/OOP/Inheritance/example3/Test5.java
@@ -0,0 +1,45 @@
+package baseJava_lessons.OOP.Inheritance.example3;
+
+public class Test5 {
+}
+
+interface I1 {
+    void abc();
+
+    // Если нужно оверайдить 10 классах из 100, поэтому с default
+    // default - не имеет отношение к access modifier
+    // default method - должен иметь тело
+    default void ghi() {
+        System.out.println("eto method ghi");
+    }
+
+    // static method не наследуются, поэтому чтожно вызвать
+    // только с помощью тип данных этого интерфейса, где создан
+    // public проставит java перед методом
+    static void def() {
+        System.out.println("eto static method def");
+    }
+
+    interface I5 {
+        static void fed() {
+            System.out.println("eto static method def");
+        }
+
+    }
+
+
+    class R implements I1, I5 {
+
+        public void abc() {
+            System.out.println("eto method abc");
+        }
+
+        public static void main(String[] args) {
+            R z = new R();
+            z.abc();
+            z.ghi();
+            I1.def();
+            I5.fed();
+        }
+    }
+}
