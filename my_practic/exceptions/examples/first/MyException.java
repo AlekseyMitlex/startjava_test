63b6619bd3020d9824de1f3ee3e5696dfd2ce19d
AlekseyMitlex <aamtr@yandex.ru>
2022-01-25 08:33:00 +0700

    add exceptions.examples.first

---
+++ my_practic/exceptions/examples/first/MyException.java
@@ -0,0 +1,23 @@
+package exceptions.examples.first;
+
+public class MyException {
+
+    public static void main(String[] args) {
+        int catNumber;
+        int zero;
+//        catNumber = 1; // у меня один кот
+//        zero = 0; // ноль, он и в Африке ноль
+//        int result = catNumber / zero;
+
+        try { // мониторим код
+            catNumber = 1; // у меня один кот
+            zero = 0; // ноль, он и в Африке ноль
+            int result = catNumber / zero;
+            System.out.println("Не увидите это сообщение!");
+        } catch (ArithmeticException e) {
+            System.out.println("Увидите это сообщение! " + e);
+            ;
+        }
+        System.out.println("Жизнь продолжается");
+    }
+}
