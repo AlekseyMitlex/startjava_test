5f796c605c04687d39520e9816ee5b691b9ca30a
AlekseyMitlex <aamtr@yandex.ru>
2022-01-27 06:44:50 +0700

    fix LocalDateTime + another method();

--- my_practic/localdatetime/LocalDateTimeDuration.java
+++ my_practic/localdatetime/LocalDateTimeDuration.java
@@ -7,7 +7,8 @@ public class LocalDateTimeDuration {
 
     public static void main(String[] args) {
 
-        // Duration Работает только с LocalTime и LocaDateTime
+        // Duration Работает только с LocalTime и LocaDateTime месяц+ через Period
+        // Дни/час/минута/секунда/милиСек/наноСек
         LocalDateTime nahcalo = LocalDateTime.of(2014, Month.SEPTEMBER, 1, 16, 40);
         Period p = Period.ofMonths(3);
         LocalTime lt = LocalTime.of(10, 30);
