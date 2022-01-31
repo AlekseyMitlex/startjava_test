4fc56fdb398d2539e06fee6cd5c96e62faf5d804
AlekseyMitlex <aamtr@yandex.ru>
2022-01-31 08:51:25 +0700

    add annotation(Ex1 Overriding)

---
+++ my_practic/annotation/ex1/Test1.java
@@ -0,0 +1,35 @@
+package annotation.ex1;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+        Parent p = new Parent("Zaur");
+        p.showInfo();
+    }
+}
+
+class Parent {
+    String name;
+
+    public Parent(String name) {
+        this.name = name;
+    }
+
+    void showInfo() {
+        System.out.println("It`s Parent class. Name = " + name);
+    }
+}
+
+class Child extends Parent {
+
+    public Child(String name) {
+        super(name);
+    }
+
+    // @Override Аннотация не даёт допустить случайные ошибки и проверяет правильность
+    // перезаписывания, даже орфографию
+    @Override
+    void showInfo() {
+        System.out.println("It`s Child class. Name = " + name);
+    }
+}
