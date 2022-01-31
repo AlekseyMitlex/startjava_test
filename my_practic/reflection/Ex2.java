950ba2a6287cc0f39b5d6b073ab42a17184eb9d4
AlekseyMitlex <aamtr@yandex.ru>
2022-01-31 07:37:07 +0700

    add Ex1 reflection

--- my_practic/reflection/Ex2.java
+++ my_practic/reflection/Ex2.java
@@ -10,17 +10,22 @@ public class Ex2 {
             , NoSuchMethodException, InvocationTargetException {
         Class employeeClass = Class.forName("reflection.Employee");
 
+        // Устаревший способ newInstance();
 //        Employee o = (Employee)employeeClass.newInstance();
 //        System.out.println(o);
 
+        // Конструктор с Generics <Employee>
         Constructor<Employee> constructor1 = employeeClass.getConstructor();
         Employee obj1 = constructor1.newInstance();
 
+        // Конструктор с 3 параметрами
         Constructor constructor2 = employeeClass.getConstructor(int.class, String.class, String.class);
         Object obj2 = constructor2.newInstance(5, "Zaur", "IT");
         System.out.println(obj2);
 
+        // Вызов метода с помощью рефлексии
         Method method = employeeClass.getMethod("setSalary", double.class);
+        // Запуск этого метода на объект obj2 с з/п 800
         method.invoke(obj2, 800.88);
         System.out.println(obj2);
 
---
+++ my_practic/reflection/Ex3.java
@@ -0,0 +1,31 @@
+package reflection;
+
+import java.lang.reflect.Field;
+
+// Декларируем гет/сет и конструктор на 4 параметра для Салари(инициализирует 100)
+// Рефлексия нарушает правила инкапсуляции. Попробуем прочитать Салари!!!
+
+public class Ex3 {
+
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
+        Employee employee = new Employee(10, "Zaur", "IT");
+
+        // 2-й способ вызова Class
+        Class employeeClass = employee.getClass();
+
+        // Get field salary через getDeclaredField
+        Field field = employeeClass.getDeclaredField("salary");
+
+        // Требуем получить доступ к private Field
+        field.setAccessible(true);
+        // Пробуем записать значение Field записать в переменную
+        double salaryValue = (Double) field.get(employee);
+        System.out.println("salaryValue = " + salaryValue);
+
+        // Присвоили з/п работнику 1500
+        // т.о. Рефлекшион обходит инкапсуляцию
+        field.set(employee, 1500);
+        System.out.println("employee = " + employee);
+
+    }
+}
