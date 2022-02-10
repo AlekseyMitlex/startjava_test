7aa34c768dd7c1a1e048e9a467b528a38191b643
AlekseyMitlex <aamtr@yandex.ru>
2022-02-10 14:31:02 +0700

    fix

--- my_practic/array_example/ArraysCopy.java
+++ my_practic/array_example/ArraysCopy.java
@@ -5,14 +5,17 @@ import java.util.Arrays;
 public class ArraysCopy {
 
     public static void main(String[] args) {
-        int index = 0;
+        int insertIdx = -(-1) - 1;
         int[] array = {2, 5, 6, 7, 11, 13, 18};
         int[] array1 = {0, 0, 0, 0, 0, 0, 0};
 
         System.out.println(Arrays.toString(array));
         System.out.println(Arrays.toString(array1));
-        System.arraycopy(array, index + 1, array1, index, array.length - index - 1);
+        System.arraycopy(array, insertIdx, array1, insertIdx + 1, array.length - insertIdx - 1);
         System.out.println(Arrays.toString(array));
+        System.out.println(Arrays.toString(array1));
+        insertIdx = -(-0) - 1;
+        System.arraycopy(array, insertIdx, array1, insertIdx + 1, array.length - insertIdx - 1);
         System.out.println(Arrays.toString(array1));
     }
 }
