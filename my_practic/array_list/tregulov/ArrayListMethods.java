42bf5946aec6d1cacaf136e5faef6db502146957
AlekseyMitlex <aamtr@yandex.ru>
2022-02-12 11:49:49 +0700

    add Tregulov ArrayList part 3 + literator

--- my_practic/array_list/tregulov/ArrayListMethods.java
+++ my_practic/array_list/tregulov/ArrayListMethods.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-public class Test2 {
+public class ArrayListMethods {
 
     public static void main(String[] args) {
         ArrayList<String> list = new ArrayList<>();
